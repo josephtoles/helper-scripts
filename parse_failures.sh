25d78e205c040d26cdda870ee27e07b1cb4fb0db
Joseph Toles <jbtoles@gmail.com>
2015-06-22 13:42:14 -0700

    Added '-a' option to parse_failures.sh

--- parse_failures.sh
+++ parse_failures.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
+# Run with
+# yes yes | ./manage.py test api.tests.test_comment 2>&1| parse_failures.sh
+# You may want to make an alias for this
+
 # This script simplifies the readout of Django unit tests so that you know
 # exactly what unit tests failed
 # Standard input is the unit test so you can do
 # $ ./manage.py test | parse_failures.sh
+
+# TODO handle arguments properly
+if [[ $1 == '-a' ]]
+then
+    echo 'running all unit tests'
+    yes yes | ./manage.py test api 2>&1| parse_failures.sh
+    exit 0
+fi
 
 while read input
 do
